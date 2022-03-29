33cf0d837116f922b920c368a820879ab6093c01
Jarin Akter <jarinakter2711@gmail.com>
2022-03-30 01:02:27 +0600

    my 1st practice

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import About from "./component/About/About";
+import Home from "./component/Home/Home";
+
+
+function App() {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1>This is main part</h1>
+      <Home />
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/About/About.js
@@ -0,0 +1,10 @@
+function About() {
+    return (
+      <div style={{textAlign:"center", color:"green"}}>
+        <h1>This is About</h1>     
+      </div>
+    );
+  }
+  
+  export default About;
+  
